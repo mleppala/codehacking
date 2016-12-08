554c46ac8d75f06433b6bce6be3db8a558a2830c
Michael Leppala <milco@mi.rr.com>
2016-12-08 17:42:38 -0500

    Added added role and status to the view user form

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -8,6 +8,18 @@
         {!! Form::label('name', 'Name:') !!}
         {!! Form::text('name', null, ['class'=>'form-control']) !!}
     </div>
+    <div class="form-group">
+        {!! Form::label('email', 'Email:') !!}
+        {!! Form::email('email', null, ['class'=>'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::label('role_id', 'Role:') !!}
+        {!! Form::text('role_id', null, ['class'=>'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::label('is_active', 'Status:') !!}
+        {!! Form::text('is_active', null, ['class'=>'form-control']) !!}
+    </div>
 
     <div class="form-group">
         {!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
